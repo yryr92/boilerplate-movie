43f58b284a5a662284f6c40fd5ff0aaa1d83f6c1
unknown <rin217@naver.com>
2023-11-07 16:48:59 +0900

    movie detail

---
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react'
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config'
+import MainImage from '../LandingPage/Sections/MainImage'
+import MovieInfo from './Sections/MovieInfo'
+import GridCard from '../commons/GridCard'
+import { Row } from 'antd';
+
+function MovieDetail(props) {
+
+    let movieId = props.match.params.movieId
+    const [Movie, SetMovie] = useState([])
+    const [Casts, SetCasts] = useState([])
+    const [ActorToggle, SetActorToggle] = useState(false)
+
+    useEffect(() => {
+
+        console.log(props.match)
+
+        //const endpoint = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+
+        let endpointCrew = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`
+        let endpointInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`
+
+        fetch(endpointInfo)
+            .then(response => response.json())
+            .then(response => {
+                console.log('movie', response)
+                SetMovie(response)
+            })
+
+        fetch(endpointCrew)
+            .then(response => response.json())
+            .then(response => {
+                console.log('crew', response)
+                SetCasts(response.cast)
+            })
+
+    }, [])
+
+    const toggleActorView = () => {
+        SetActorToggle(!ActorToggle)
+    }
+
+
+    return (
+        <div>
+
+            <MainImage
+                image={`${IMAGE_BASE_URL}w1280${Movie.backdrop_path}`}
+                title={Movie.original_title}
+                text={Movie.overview}
+            />
+
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+                <MovieInfo
+                    movie={Movie}
+                />
+
+                <br />
+
+                <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem' }}>
+                    <button onClick={toggleActorView}>Toggle Action View</button>
+                </div>
+
+                {ActorToggle &&
+                    <Row gutter={[16, 16]}>
+                        {Casts && Casts.map((cast, index) => (
+                            <React.Fragment key={index}>
+                                <GridCard
+                                    image={cast.profile_path ? `${IMAGE_BASE_URL}w500${cast.profile_path}` : null}
+                                    characterName={cast.name}
+                                />
+                            </React.Fragment>
+                        ))}
+                    </Row>
+                }
+
+                
+
+            </div>
+
+        </div>
+    )
+}
+
+export default MovieDetail
--- client/src/components/views/commons/GridCard.js
+++ client/src/components/views/commons/GridCard.js
@@ -2,14 +2,25 @@ import React from "react"
 import { Col } from 'antd';
 
 function GridCard(props) {
-  return (
-    <Col lg={6} md={8} xs={24}>
-        <div style={{position:'relative'}}>
-            <a href={`/movie/${props.movieId}`}></a>
-            <img style={{width:'100%', height:'320px'}} src={props.image} alt={props.movieName}/>
-        </div>
-    </Col>
-  )
+  if(props.landingPage) {
+    return (
+      <Col lg={6} md={8} xs={24}>
+          <div style={{position:'relative'}}>
+              <a href={`/movie/${props.movieId}`}>
+              <img style={{width:'100%', height:'320px'}} src={props.image} alt={props.movieName}/>
+              </a>
+          </div>
+      </Col>
+    )
+  } else {
+    return (
+      <Col lg={6} md={8} xs={24}>
+          <div style={{position:'relative'}}>
+              <img style={{width:'100%', height:'320px'}} src={props.image} alt={props.characterName}/>
+          </div>
+      </Col>
+    )
+  }
 }
 
 export default GridCard
